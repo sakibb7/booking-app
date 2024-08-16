d4ede0339a37ff8b476d808da263ceb121bdb393
sakibb7 <sakibb076@gmail.com>
2024-08-16 12:55:22 +0600

    added second db for e2e test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,10 @@ import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+// .then(()=> {
+//   console.log("Connected to: ",process.env.MONGODB_CONNECTION_STRING )
+// })
 
 const app = express();
 app.use(cookieParser());
