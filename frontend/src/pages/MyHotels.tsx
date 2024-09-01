b68c04a9b69c1cba7037fffa963e3a9d1e62656e
sakibb7 <sakibb076@gmail.com>
2024-09-01 09:56:56 +0600

    designed my hotel card

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -31,29 +31,32 @@ export default function MyHotels() {
           </Link>
         </div>
 
-        <div className="py-8 flex flex-col gap-6">
+        <div className="py-8 gap-6 grid grid-cols-12">
           {hotelData.map((hotel, idx) => (
-            <div className="flex justify-start items-center gap-3" key={idx}>
-              <div className="h-[200px] w-[150px]">
+            <div
+              className="flex justify-start items-center gap-3 col-span-4 flex-col"
+              key={idx}
+            >
+              <div className="h-[300px] self-stretch">
                 <img
                   src={hotel.imageUrls[0]}
-                  className="object-cover h-full w-full"
+                  className=" object-cover w-full h-full rounded-t-xl "
                 />
               </div>
-              <div className="flex flex-col gap-3">
-                <h2 className="text-2xl font-semibold">{hotel.name}</h2>
-                <div className="flex justify-start items-center text-sm gap-1">
+              <div className="flex flex-col gap-3 border border-neutral-300 rounded-xl p-6 -mt-20 bg-white text-neutral-500">
+                <h2 className="text-2xl font-semibold  text-slate-700">
+                  {hotel.name}
+                </h2>
+                <div className="flex justify-start items-center text-sm gap-1 text-slate-700">
                   <PiMapPin />
                   <p>
                     {hotel.city}, {hotel.country}
                   </p>
                 </div>
-                <p>{hotel.description.slice(0, 150)}</p>
-                <p className="text-xl font-bold text-blue-600">
-                  Price: {hotel.pricePerNight}
-                </p>
+                <p className="text-sm">{hotel.description.slice(0, 130)}</p>
+
                 <div className="flex justify-start items-center gap-2">
-                  {hotel.facilities.map((item, idx) => (
+                  {hotel.facilities.slice(0, 3).map((item, idx) => (
                     <p
                       key={idx}
                       className="py-0.5 px-2 border rounded-full text-xs"
@@ -62,7 +65,13 @@ export default function MyHotels() {
                     </p>
                   ))}
                 </div>
-                <div className="flex justify-start">
+                <div className="flex justify-between items-center pt-4">
+                  <p className="text-3xl font-medium text-slate-800">
+                    ${hotel.pricePerNight}{" "}
+                    <span className="text-slate-500 text-sm font-medium">
+                      /person
+                    </span>
+                  </p>
                   <Link
                     to={`/edit-hotel/${hotel._id}`}
                     className="bg-blue-600 text-white px-6 py-1 rounded-full"
