0b833eecfc0662047562771d11aa36922993b214
sakibb7 <sakibb076@gmail.com>
2024-09-02 22:25:52 +0600

    updated sing in & sign up page

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,9 +1,10 @@
-import Layouts from "../layouts/Layouts";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../context/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import signInBg from "../assets/images/sign-in-img.jpg";
+import logo from "../assets/images/icon.png";
 
 export type SignInFormData = {
   email: string;
@@ -39,59 +40,76 @@ function SignInPage() {
   });
 
   return (
-    <Layouts>
-      <div className="container flex justify-center items-center">
-        <form onSubmit={onSubmit} className="pt-12 w-2/3 flex flex-col gap-5">
-          <h2 className="text-3xl font-semibold">Sign In</h2>
-
-          <label className="w-full">
-            Email
-            <div className="border border-gray-500 rounded-md p-2">
-              <input
-                type="email"
-                placeholder="example@mail.com"
-                className="w-full outline-none bg-transparent text-gray-700"
-                {...register("email", {
-                  required: "This field is required",
-                })}
-              />
-            </div>
-            {errors.email && (
-              <span className="text-red-500">{errors.email.message}</span>
-            )}
-          </label>
-          <label className="w-full">
-            Password
-            <div className="border border-gray-500 rounded-md p-2">
-              <input
-                type="password"
-                placeholder="password"
-                className="w-full outline-none bg-transparent text-gray-700"
-                {...register("password", {
-                  required: "This field is required",
-                  minLength: {
-                    value: 6,
-                    message: "Password must be at least 6 charectersd",
-                  },
-                })}
-              />
-            </div>
-            {errors.password && (
-              <span className="text-red-500">{errors.password.message}</span>
-            )}
-          </label>
-
-          <div className="">
-            <button
-              type="submit"
-              className="text-white bg-yellow-500 px-6 py-3 rounded-md"
-            >
-              Sign In
-            </button>
+    <div className=" h-screen overflow-hidden relative">
+      <img src={signInBg} alt="" />
+      <div className="absolute inset-0 flex justify-center items-center">
+        <div className=" flex justify-center items-start flex-col p-12 rounded-xl bg-white w-[500px]">
+          <div className="flex justify-start items-center">
+            <img src={logo} alt="" className="size-10" />
+            <Link to={"/"} className="text-xl font-semibold  ">
+              Bookingly
+            </Link>
           </div>
-        </form>
+          <p className="text-3xl font-bold text-slate-700 text-start pt-4">
+            {" "}
+            Welcome back
+          </p>
+          <p className="text-sm text-slate-700 pt-1">
+            New here?{" "}
+            <Link to={"/sign-up"} className="text-p1 underline">
+              Create an acount
+            </Link>
+          </p>
+          <form onSubmit={onSubmit} className="pt-6 flex flex-col gap-4 w-full">
+            <label className="w-full text-slate-500 text-sm">
+              Enter email
+              <div className="border border-slate-300 rounded-md p-2 mt-1">
+                <input
+                  type="email"
+                  placeholder="example@mail.com"
+                  className=" outline-none bg-transparent text-gray-700 w-full"
+                  {...register("email", {
+                    required: "This field is required",
+                  })}
+                />
+              </div>
+              {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
+              )}
+            </label>
+            <label className="w-full text-slate-500 text-sm">
+              Password
+              <div className="border border-slate-300 rounded-md p-2 mt-1">
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="w-full outline-none bg-transparent text-gray-700"
+                  {...register("password", {
+                    required: "This field is required",
+                    minLength: {
+                      value: 6,
+                      message: "Password must be at least 6 charectersd",
+                    },
+                  })}
+                />
+              </div>
+              {errors.password && (
+                <span className="text-red-500">{errors.password.message}</span>
+              )}
+            </label>
+
+            <div className="pt-2">
+              <button
+                type="submit"
+                className="text-white bg-p1 px-6 py-2 rounded-md text-sm font-medium"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </Layouts>
+    </div>
   );
 }
 
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,9 +1,10 @@
 import { useForm } from "react-hook-form";
-import Layouts from "../layouts/Layouts";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../context/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import signInBg from "../assets/images/sign-in-img.jpg";
+import logo from "../assets/images/icon.png";
 
 export type RegisterFormData = {
   firstName: string;
@@ -41,121 +42,142 @@ function SignUpPage() {
     mutate(data);
   });
   return (
-    <Layouts>
-      <div className="container flex justify-center items-center">
-        <form onSubmit={onSubmit} className="pt-12 w-2/3 flex flex-col gap-5">
-          <h2 className="text-3xl font-semibold">Create an Account</h2>
-
-          <div className="flex flex-col md:flex-row gap-5 pt-8 w-full">
-            <label className="w-full">
-              First Name
-              <div
-                className={`border ${
-                  errors.firstName ? "border-red-500" : "border-gray-500"
-                } rounded-md p-2`}
-              >
-                <input
-                  type="text"
-                  placeholder="First Name"
-                  className="w-full outline-none bg-transparent text-gray-700"
-                  {...register("firstName", {
-                    required: "This field is required",
-                  })}
-                />
-              </div>
-              {errors.firstName && (
-                <span className="text-red-500">{errors.firstName.message}</span>
-              )}
-            </label>
-            <label className="w-full">
-              Last Name
+    <div className=" h-screen overflow-hidden relative">
+      <img src={signInBg} alt="" />
+      <div className="absolute inset-0 flex justify-center items-center">
+        <div className=" flex justify-center items-start flex-col p-12 rounded-xl bg-white w-[500px]">
+          <div className="flex justify-start items-center">
+            <img src={logo} alt="" className="size-10" />
+            <Link to={"/"} className="text-xl font-semibold  ">
+              Bookingly
+            </Link>
+          </div>
+          <p className="text-3xl font-bold text-slate-700 text-start pt-4">
+            {" "}
+            Create an account
+          </p>
+          <p className="text-sm text-slate-700 pt-1">
+            Already a member?{" "}
+            <Link to={"/sign-in"} className="text-p1 underline">
+              Sign in
+            </Link>
+          </p>
+          <form onSubmit={onSubmit} className="pt-6 flex flex-col gap-4">
+            <div className="flex flex-col md:flex-row gap-4 w-full">
+              <label className="w-full text-slate-500 text-sm">
+                First Name
+                <div
+                  className={`border ${
+                    errors.firstName ? "border-red-500" : "border-gray-500"
+                  } rounded-md p-2`}
+                >
+                  <input
+                    type="text"
+                    placeholder="First Name"
+                    className="w-full outline-none bg-transparent text-gray-700"
+                    {...register("firstName", {
+                      required: "This field is required",
+                    })}
+                  />
+                </div>
+                {errors.firstName && (
+                  <span className="text-red-500">
+                    {errors.firstName.message}
+                  </span>
+                )}
+              </label>
+              <label className="w-full text-slate-500 text-sm">
+                Last Name
+                <div className="border border-gray-500 rounded-md p-2">
+                  <input
+                    type="text"
+                    placeholder="Last Name"
+                    className="w-full outline-none bg-transparent text-gray-700"
+                    {...register("lastName", {
+                      required: "This field is required",
+                    })}
+                  />
+                </div>
+                {errors.lastName && (
+                  <span className="text-red-500">
+                    {errors.lastName.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <label className="w-full text-slate-500 text-sm">
+              Email
               <div className="border border-gray-500 rounded-md p-2">
                 <input
-                  type="text"
-                  placeholder="Last Name"
+                  type="email"
+                  placeholder="example@mail.com"
                   className="w-full outline-none bg-transparent text-gray-700"
-                  {...register("lastName", {
+                  {...register("email", {
                     required: "This field is required",
                   })}
                 />
               </div>
-              {errors.lastName && (
-                <span className="text-red-500">{errors.lastName.message}</span>
+              {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
               )}
             </label>
-          </div>
-          <label className="w-full">
-            Email
-            <div className="border border-gray-500 rounded-md p-2">
-              <input
-                type="email"
-                placeholder="example@mail.com"
-                className="w-full outline-none bg-transparent text-gray-700"
-                {...register("email", {
-                  required: "This field is required",
-                })}
-              />
-            </div>
-            {errors.email && (
-              <span className="text-red-500">{errors.email.message}</span>
-            )}
-          </label>
-          <label className="w-full">
-            Password
-            <div className="border border-gray-500 rounded-md p-2">
-              <input
-                type="password"
-                placeholder="password"
-                className="w-full outline-none bg-transparent text-gray-700"
-                {...register("password", {
-                  required: "This field is required",
-                  minLength: {
-                    value: 6,
-                    message: "Password must be at least 6 charectersd",
-                  },
-                })}
-              />
-            </div>
-            {errors.password && (
-              <span className="text-red-500">{errors.password.message}</span>
-            )}
-          </label>
-          <label className="w-full">
-            Confirm Password
-            <div className="border border-gray-500 rounded-md p-2">
-              <input
-                type="password"
-                placeholder="password"
-                className="w-full outline-none bg-transparent text-gray-700"
-                {...register("confirmPassword", {
-                  validate: (val) => {
-                    if (!val) {
-                      return "This Field is required";
-                    } else if (watch("password") !== val) {
-                      return "Password doesn't match ";
-                    }
-                  },
-                })}
-              />
-            </div>
-            {errors.confirmPassword && (
-              <span className="text-red-500">
-                {errors.confirmPassword.message}
-              </span>
-            )}
-          </label>
+            <label className="w-full text-slate-500 text-sm">
+              Password
+              <div className="border border-gray-500 rounded-md p-2">
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="w-full outline-none bg-transparent text-gray-700"
+                  {...register("password", {
+                    required: "This field is required",
+                    minLength: {
+                      value: 6,
+                      message: "Password must be at least 6 charectersd",
+                    },
+                  })}
+                />
+              </div>
+              {errors.password && (
+                <span className="text-red-500">{errors.password.message}</span>
+              )}
+            </label>
+            <label className="w-full text-slate-500 text-sm">
+              Confirm Password
+              <div className="border border-gray-500 rounded-md p-2">
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="w-full outline-none bg-transparent text-gray-700"
+                  {...register("confirmPassword", {
+                    validate: (val) => {
+                      if (!val) {
+                        return "This Field is required";
+                      } else if (watch("password") !== val) {
+                        return "Password doesn't match ";
+                      }
+                    },
+                  })}
+                />
+              </div>
+              {errors.confirmPassword && (
+                <span className="text-red-500">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+            </label>
 
-          <div className="">
-            <button
-              type="submit"
-              className="text-white bg-yellow-500 px-6 py-3 rounded-md"
-            >
-              Create Account
-            </button>
-          </div>
-        </form>
+            <div className="pt-2">
+              <button
+                type="submit"
+                className="text-white bg-p1 px-6 py-2 rounded-md text-sm font-medium"
+              >
+                Create account
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </Layouts>
+    </div>
   );
 }
 
